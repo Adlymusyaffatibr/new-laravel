c8d41bccbcb013ca8ab2bebf4ba8157d7a124b0f
adly musyaffa tibr <adlymusyaffatibr@smkwikrama.sch.id>
2024-06-20 19:38:58 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/projek', function () {
+    return view('projects');
+});
+Route::get('/resume', function () {
+    return view('resume');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
